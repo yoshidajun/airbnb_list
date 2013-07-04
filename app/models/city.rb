c6f89252f44751bc79c5a805d4c9efb94c74c744
Jun Yoshida <yoshida.jun@gmail.com>
2013-07-03 17:25:48 -0700

    [add] City M: Todo comment to get normalized address data from AirBnB

--- app/models/city.rb
+++ app/models/city.rb
@@ -7,4 +7,7 @@ class City < ActiveRecord::Base
   # * normalize city name
   # ** e.g. "SAN FraNCiSO, CA" => "san francisco, ca"
   # * how to recognize "san francisco ca" and "san francisco, ca" are same?
+  #
+  # => use *hash['breadcrumbs'].map(&:first)* to get normalied address data.
+  # e.g. ["United States", "California", "San Francisco"]
 end
